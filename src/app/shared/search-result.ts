1d33e2106ad3ead21aad8c2b093acdba253c70a9
Amy Egan <jaimes978@outlook.com>
2020-06-06 00:20:36 -0400

    Map user details

--- src/app/shared/search-result.ts
+++ src/app/shared/search-result.ts
@@ -1,7 +1,9 @@
 import { User } from './user';
+import { UserDetails } from './user-details';
 
 export interface SearchResult {
   total_count: number,
   incomplete_results: boolean,
   items: Array<User>
+  users?: Array<UserDetails>
 }
